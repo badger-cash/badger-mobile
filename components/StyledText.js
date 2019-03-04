2d590a4677199e7668f33df5eb271338ee62bd82
Peter Zenger <peter.zenger1@gmail.com>
2019-03-04 11:57:38 -0800

    Tool setup

---
+++ components/StyledText.js
@@ -0,0 +1,17 @@
+// @flow
+
+import React from "react";
+import { Text } from "react-native";
+
+class MonoText extends React.Component {
+  render() {
+    return (
+      <Text
+        {...this.props}
+        style={[this.props.style, { fontFamily: "space-mono" }]}
+      />
+    );
+  }
+}
+
+export { MonoText };
