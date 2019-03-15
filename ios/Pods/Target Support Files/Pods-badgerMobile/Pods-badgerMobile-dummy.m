141f01fb5f9bdad4bce7da3ac9481b06c30b9501
Peter Zenger <peter.zenger1@gmail.com>
2019-03-15 15:59:48 -0700

    0.59

---
+++ ios/Pods/Target Support Files/Pods-badgerMobile/Pods-badgerMobile-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_badgerMobile : NSObject
+@end
+@implementation PodsDummy_Pods_badgerMobile
+@end
--- ios/badgerMobileTests/badgerMobileTests.m
+++ ios/badgerMobileTests/badgerMobileTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface badger2Tests : XCTestCase
+@interface badgerMobileTests : XCTestCase
 
 @end
 
-@implementation badger2Tests
+@implementation badgerMobileTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
