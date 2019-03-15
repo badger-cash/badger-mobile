e4e98e894d655d0c8e03f8a96483af21f40b315e
Peter Zenger <peter.zenger1@gmail.com>
2019-03-15 10:59:35 -0700

    browser error

--- App.js
+++ App.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 
+
+import BITBOXSDK from 'bitbox-sdk'
+const BITBOX = new BITBOXSDK;
+
+const deriveAccount = (
+  mnemonic = null,
+  accountIndex = 0,
+  childIndex = 0,
+  hdPathString = "m/44'/245'/0'"
+) => {
+  if (!mnemonic) {
+    mnemonic = BITBOX.Mnemonic.generate(128);
+  }
+
+  const seed = BITBOX.Mnemonic.toSeed(mnemonic);
+  const hdWallet = BITBOX.HDNode.fromSeed(seed, "mainnet");
+  const rootNode = BITBOX.HDNode.derivePath(hdWallet, hdPathString);
+
+  const child = BITBOX.HDNode.derivePath(
+    this.root,
+    `/{accountIndex}'/0/${childIndex}`
+  );
+  const keypair = BITBOX.HDNode.toKeyPair(child);
+  const address = BITBOX.ECPair.toCashAddress(keypair);
+
+  return { mnemonic, keypair, address };
+};
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
@@ -8,9 +36,11 @@ const instructions = Platform.select({
 
 export default class App extends Component {
   render() {
+    const account = deriveAccount();
+    console.log('account derived');
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to React Native????</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
