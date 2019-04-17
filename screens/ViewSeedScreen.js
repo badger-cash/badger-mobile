c77a8bbe3ada136adb1b8b188b51c9a8d48f15c5
Peter Zenger <peter.zenger1@gmail.com>
2019-04-17 15:24:36 -0700

    Seed phrase better security

--- screens/ViewSeedScreen.js
+++ screens/ViewSeedScreen.js
@@ -1,16 +1,35 @@
 // @flow
 
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
 
-import { Text, View, SafeAreaView } from "react-native";
+import { View, SafeAreaView } from "react-native";
+import { NavigationEvents } from "react-navigation";
 
 import { getMnemonicSelector } from "../data/accounts/selectors";
-import { T, H1, Spacer } from "../atoms";
+import { T, H1, Spacer, Button } from "../atoms";
 
 const Screen = styled(View)`
   padding: 10px;
+`;
+
+const WordHolder = styled(View)`
+  position: relative;
+`;
+
+const Cover = styled(View)`
+  position: absolute;
+  background-color: rgba(255, 255, 255, 0.95);
+  align-items: center;
+  justify-content: center;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  height: 100%;
+  width: 100%;
+  z-index: 1;
 `;
 
 // TODO - Currently stays on this screen if navigating away using bottom nav.
@@ -19,10 +38,14 @@ const Screen = styled(View)`
 type Props = { mnemonic: string };
 
 const ViewSeedScreen = ({ mnemonic }: Props) => {
-  const separated = mnemonic.split(" ");
+  const [showing, setShowing] = useState(false);
+
+  const words = showing ? mnemonic : "---------- ".repeat(12).trim();
+  const separated = words.split(" ");
 
   return (
     <SafeAreaView>
+      <NavigationEvents onWillBlur={() => setShowing(false)} />
       <Screen>
         <H1>Backup Seed Phrase</H1>
         <Spacer />
@@ -31,11 +54,24 @@ const ViewSeedScreen = ({ mnemonic }: Props) => {
           keep it very, very private. This is your private key.
         </T>
         <Spacer />
-        {separated.map((word, idx) => (
-          <T key={idx}>
-            {idx + 1}. {word}
-          </T>
-        ))}
+        <WordHolder>
+          {!showing && (
+            <Cover>
+              <T center>I am in a private area and wish to see my seed words</T>
+              <Spacer />
+              <Button
+                text="Reveal Seed Words"
+                onPress={() => setShowing(true)}
+                nature="cautionGhost"
+              />
+            </Cover>
+          )}
+          {separated.map((word, idx) => (
+            <T key={idx}>
+              {idx + 1}. {word}
+            </T>
+          ))}
+        </WordHolder>
       </Screen>
     </SafeAreaView>
   );
