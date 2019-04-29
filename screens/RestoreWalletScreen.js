a00da1b3919de59f938af50a8cd66b772957f400
Peter Zenger <peter.zenger1@gmail.com>
2019-04-29 15:54:32 -0700

    Improving the mnemonic input experience

--- screens/RestoreWalletScreen.js
+++ screens/RestoreWalletScreen.js
@@ -4,9 +4,9 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { connect } from "react-redux";
 
-import { SafeAreaView, View, TextInput, TouchableOpacity } from "react-native";
+import { SafeAreaView, View, TextInput } from "react-native";
 
-import { H1, H2, Button, T, Spacer } from "../atoms";
+import { H1, Button, T, Spacer } from "../atoms";
 import { getAccount } from "../data/accounts/actions";
 import { hasMnemonicSelector } from "../data/accounts/selectors";
 
@@ -19,6 +19,33 @@ const StyledTextInput = styled(TextInput)`
   padding: 15px 5px;
 `;
 
+const ErrorContainer = styled(View)`
+  border-color: ${props => props.theme.danger500};
+  border-width: 1px;
+  padding: 5px;
+`;
+
+const formatMnemonic = (mnemonic: string) => {
+  if (!mnemonic) return "";
+  const splitWords = mnemonic.split(" ");
+
+  // Remove all extra spaces whenever a new word begins
+  const cleaned =
+    splitWords.slice(-1)[0] !== "" ? splitWords.filter(Boolean) : splitWords;
+
+  const formatted = cleaned.join(" ").toLowerCase();
+
+  return formatted;
+};
+
+// Basic mnemonic validation, for now just ensure it's length is 12
+const validateMnemonic = (mnemonic: string) => {
+  const trimmed = mnemonic.trim();
+  const split = trimmed.split(" ");
+  const validLength = split.length === 12;
+  return validLength;
+};
+
 type Props = {
   getAccount: Function,
   isCreated: boolean,
@@ -27,6 +54,7 @@ type Props = {
 
 const RestoreWalletScreen = ({ navigation, getAccount, isCreated }: Props) => {
   const [mnemonic, setMnemonic] = useState("");
+  const [inputError, setInputError] = useState(null);
 
   useEffect(() => {
     if (isCreated) {
@@ -44,7 +72,7 @@ const RestoreWalletScreen = ({ navigation, getAccount, isCreated }: Props) => {
           Enter your 12 word recovery phrase or mnemonic to login to an existing
           account
         </T>
-        <Spacer />
+        <Spacer large />
         <StyledTextInput
           multiline
           editable
@@ -53,17 +81,40 @@ const RestoreWalletScreen = ({ navigation, getAccount, isCreated }: Props) => {
           placeholder="Enter Backup Phrase / Mnemonic"
           value={mnemonic}
           onChangeText={text => {
-            setMnemonic(text);
+            const sanitized = formatMnemonic(text);
+            setMnemonic(sanitized);
+            setInputError(null);
           }}
         />
-        <Spacer />
-        <TouchableOpacity
+        <Spacer large />
+
+        {inputError ? (
+          <>
+            <ErrorContainer>
+              <T size="small" type="danger" center>
+                {inputError}
+              </T>
+            </ErrorContainer>
+            <Spacer />
+          </>
+        ) : (
+          <Spacer />
+        )}
+
+        <Button
           onPress={() => {
+            const validMnemonic = validateMnemonic(mnemonic);
+            if (!validMnemonic) {
+              setInputError(
+                "That mnemonic doesn't look right, double check that pass phrase and make sure it's 12 words long and try again"
+              );
+              return;
+            }
             getAccount(mnemonic.trim());
           }}
-        >
-          <T>Restore</T>
-        </TouchableOpacity>
+          text="Restore Wallet"
+        />
+        <Spacer />
       </Screen>
     </SafeAreaView>
   );
