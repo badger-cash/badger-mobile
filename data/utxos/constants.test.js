c8857e5ecb76d0848be338808620555c189a9575
Peter Zenger <peter.zenger1@gmail.com>
2019-05-02 11:07:53 -0700

    package updates and very very basic test setup

---
+++ data/utxos/constants.test.js
@@ -0,0 +1,20 @@
+// @flow
+
+import {
+  UPDATE_UTXO_START,
+  UPDATE_UTXO_SUCCESS,
+  UPDATE_UTXO_FAIL
+} from "./constants";
+
+describe("UTXO::constants", () => {
+  it("UPDATE_UTXO_START defined", () => {
+    expect(UPDATE_UTXO_START).toBeDefined();
+  });
+
+  it("UPDATE_UTXO_SUCCESS defined", () => {
+    expect(UPDATE_UTXO_SUCCESS).toBeDefined();
+  });
+  it("UPDATE_UTXO_FAIL defined", () => {
+    expect(UPDATE_UTXO_FAIL).toBeDefined();
+  });
+});
---
+++ data/utxos/reducer.test.js
@@ -0,0 +1,9 @@
+// @flow
+
+describe("UTXO::reducer", () => {
+  describe("Add UTXO", () => {
+    it("compiled", () => {
+      expect(1 + 1).toBe(2);
+    });
+  });
+});
