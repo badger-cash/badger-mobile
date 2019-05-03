83362982b007343ec3b9d1aeb3aa599f6c50b3c4
Peter Zenger <peter.zenger1@gmail.com>
2019-05-03 16:05:10 -0700

    Remove unused code

--- data/accounts/constants.js
+++ data/accounts/constants.js
@@ -3,9 +3,6 @@
 // Add an additional account from HD
 export const ADD_ACCOUNT = "badger::accounts:ADD_ACCOUNT";
 
-// Set keywords to derive everything
-// export const SET_KEYWORDS = "badger::account::SET_KEYWORDS";
-
 // Get or create an account from keywords
 export const GET_ACCOUNT_START = "badger:accounts:GET_ACCOUNT_START";
 export const GET_ACCOUNT_SUCCESS = "badger:accounts:GET_ACCOUNT_SUCCESS";
