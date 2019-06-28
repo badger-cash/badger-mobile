5b9d093bd70306bb4773303f85a6503acce58573
Peter Zenger <peter.zenger1@gmail.com>
2019-06-28 14:43:52 -0700

    iOS linking fix

--- ios/badgerMobile/AppDelegate.m
+++ ios/badgerMobile/AppDelegate.m
@@ -10,7 +10,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-#import “React/RCTLinkingManager.h”
+#import <React/RCTLinkingManager.h>
 
 @implementation AppDelegate
 
