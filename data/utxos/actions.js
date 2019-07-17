7738b06b88a5b9720ed722dc7109f61da309d3f1
P <879913+SpicyPete@users.noreply.github.com>
2019-07-17 10:56:18 -0700

    Fixing comment

--- data/utxos/actions.js
+++ data/utxos/actions.js
@@ -94,7 +94,7 @@ const refreshUtxos = async (state: FullState, address: string) => {
       const slpDecoded = decodeTxOut(utxo);
       return { ...utxo, slp: slpDecoded, spendable: false };
     } catch (e) {
-      // Handle errors here, spendable false somethings
+      // Prevent spending of unknown SLP types
       if (e.message === "Unknown token type") {
         return { ...utxo, spendable: false };
       }
