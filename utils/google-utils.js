fa908d4a961b798dd038c6ad60a77e1d2ac54a9d
Unknown <paoolkim@gmail.com>
2019-07-18 19:47:59 -0700

    constants
    remove axios.

--- utils/google-utils.js
+++ utils/google-utils.js
@@ -1,17 +1,16 @@
-import axios from "axios";
-
+const uploadUrl = "https://www.googleapis.com/upload/drive/v3";
 const baseUrl = "https://www.googleapis.com/drive/v3";
 const badgerFolderQuery = `name = "Badger" and mimeType = 'application/vnd.google-apps.folder'`;
-const BadgerDirMeta = {
+const badgerKeysQuery = `name = "BadgerKeys.json" and mimeType = 'application/json'`;
+const badgerDirMeta = {
   name: "Badger",
   description:
     "The file contained in this directory is for backing up / restoring your badger wallet funds. It's very important to never lose this file or you may potentially lose access to your Bitcoin Cash and tokens. No one will be able to retrieve or recover it for you if lost.",
   kind: "drive#file",
   mimeType: "application/vnd.google-apps.folder"
 };
-const getFile = async (accessToken, params) => {
-  let queryParams = params ? constructParams(params) : params;
 
+const boundaryString = "badgerString"; // can be anything
   try {
     const { data } = await axios.get(`${baseUrl}/files?q=${queryParams}`, {
       headers: {
