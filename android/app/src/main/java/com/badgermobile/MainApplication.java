21dba4a58898b489e094e71f08f3fe4c410f36ac
Peter Zenger <peter.zenger1@gmail.com>
2019-08-06 13:11:40 -0700

    Compiles on android

--- android/app/src/main/java/com/badgermobile/MainApplication.java
+++ android/app/src/main/java/com/badgermobile/MainApplication.java
@@ -1,11 +1,15 @@
 package com.badgermobile;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.facebook.react.PackageList;
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
+
 import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
