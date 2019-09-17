c07447aae059b417d4bfed5c78771bfeaa9cfcec
Peter Zenger <peter.zenger1@gmail.com>
2019-09-17 12:34:00 -0700

    DRY + fix issue when pasting in invalid address

--- screens/SendConfirmScreen.js
+++ screens/SendConfirmScreen.js
@@ -88,6 +88,8 @@ const SwipeContent = styled(View)`
   padding-right: 10px;
   align-items: flex-end;
   justify-content: center;
+  border-width: 3px;
+  border-color: ${props => props.theme.fg100};
   background-color: ${props =>
     props.activated ? props.theme.success500 : props.theme.pending500};
 `;
@@ -318,7 +320,7 @@ const SendConfirmScreen = ({
                 leftActionActivationDistance={
                   Dimensions.get("window").width *
                   (SWIPEABLE_WIDTH_PERCENT / 100) *
-                  0.8
+                  0.7
                 }
                 leftContent={
                   <SwipeContent activated={confirmSwipeActivated}>
