4366ffdb544d1326799f89fc6cf275d5fd1a13e4
Peter Zenger <peter.zenger1@gmail.com>
2019-10-02 15:21:06 -0700

    utils/slp-sdk-utils.js

--- utils/slp-sdk-utils.js
+++ utils/slp-sdk-utils.js
@@ -2,7 +2,7 @@
 
 import SLPSDK from "slp-sdk";
 
-const SLP = new SLPSDK();
+const SLP = new SLPSDK({ restURL: "https://badger.bchtest.net/v2/" });
 
 // Uncomment for local version
 // const SLP = new SLPSDK({ restURL: "http://localhost:3000/v2/" });
