ce2c962f881f28f9d4aa0e6e00fdd9423bb81072
Peter Zenger <peter.zenger1@gmail.com>
2019-10-03 09:27:53 -0700

    version

--- screens/MenuScreen.js
+++ screens/MenuScreen.js
@@ -156,7 +156,7 @@ const MenuScreen = ({ navigation, seedViewed, fiatCurrency }: Props) => {
         <Spacer small />
         <T center size="small" type="muted2">
           {/* Version {packageJson.version} - Beta */}
-          Version 0.11.0 - Beta
+          Version 0.11.1 - Beta
         </T>
         <Spacer small />
       </StyledScrollView>
