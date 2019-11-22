dc3f9d1f3707d448139bb45da041fa5445e90840
Peter Zenger <peter.zenger1@gmail.com>
2019-11-22 09:33:08 -0800

    remove console logs

--- api/rest.bitcoin.js
+++ api/rest.bitcoin.js
@@ -1,16 +1,15 @@
 // @flow
 
+// Communicate directly with the REST api.
+
 const API = `https://rest.bitcoin.com/v2`;
 
 const getBlockCountURL = `${API}/blockchain/getBlockCount`;
 
 const getCurrentBlockheight = async () => {
   try {
-    console.log("about to try");
     const req = await fetch(getBlockCountURL);
     const resp = req.json();
-    console.log("resp?");
-    console.log(resp);
     return resp;
   } catch (e) {
     console.warn(e);
