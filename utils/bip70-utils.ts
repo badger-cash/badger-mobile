d4d27e209aa9a64c3786204ffabb40114312031f
Peter Zenger <peter.zenger1@gmail.com>
2020-01-30 10:37:36 -0800

    Fixing bip70 comments, 76 ts errors to go

--- utils/bip70-utils.ts
+++ utils/bip70-utils.ts
@@ -49,6 +49,7 @@ const postAsArrayBuffer = (
 ): Promise<any> => {
   return new Promise((accept, reject) => {
     let req = new XMLHttpRequest();
+
     req.open("POST", url, true);
     Object.entries(headers).forEach(([key, value]) => {
       req.setRequestHeader(key, value);
@@ -113,7 +114,7 @@ const getAsArrayBuffer = (
   });
 };
 
-const txidFromHex = hex => {
+const txidFromHex = (hex: string) => {
   const buffer = Buffer.from(hex, "hex");
   const hash = SLP.Crypto.hash256(buffer).toString("hex");
   const txid = hash
@@ -123,7 +124,7 @@ const txidFromHex = hex => {
   return txid;
 };
 
-const decodePaymentResponse = async responseData => {
+const decodePaymentResponse = async (responseData: any) => {
   const buffer = await Buffer.from(responseData);
 
   // Inspired by Badger extension
@@ -131,7 +132,7 @@ const decodePaymentResponse = async responseData => {
     const responseBody = PaymentProtocol.PaymentACK.decode(buffer);
 
     const responseAck = new PaymentProtocol().makePaymentACK(responseBody);
-    // Cleanup PR for testing and review.
+
     const responseSerializedPayment = responseAck.get("payment");
     const responseDecodedPayment = await PaymentProtocol.Payment.decode(
       responseSerializedPayment
@@ -141,7 +142,6 @@ const decodePaymentResponse = async responseData => {
       responseDecodedPayment
     );
     return {
-      // Verify the request signature
       responsePayment,
       responseAck
     };
@@ -150,24 +150,24 @@ const decodePaymentResponse = async responseData => {
   }
 };
 
-const decodePaymentRequest = async (requestData): Promise<PaymentRequest> => {
+const decodePaymentRequest = async (
+  requestData: any
+): Promise<PaymentRequest> => {
   const buffer = await Buffer.from(requestData);
 
   try {
-    let body = PaymentProtocol.PaymentRequest.decode(buffer);
-
     // Get the payment details
-    let request = new PaymentProtocol().makePaymentRequest(body);
-    const detailsData = {};
+    const body = PaymentProtocol.PaymentRequest.decode(buffer);
 
-    // Verify network is mainnet
+    const request = new PaymentProtocol().makePaymentRequest(body);
+    const detailsData = {} as PaymentRequest;
+
     let serializedDetails = request.get("serialized_payment_details");
     const verifiedData = request.verify(true);
     detailsData.verified = false;
 
     if (
       verifiedData.caTrusted &&
-      // Sanity check time created is in the past
       verifiedData.chainVerified &&
       verifiedData.isChain &&
       verifiedData.selfSigned === 0 &&
@@ -183,46 +183,45 @@ const decodePaymentRequest = async (requestData): Promise<PaymentRequest> => {
       serializedDetails
     );
     const details = new PaymentProtocol().makePaymentDetails(decodedDetails);
-    detailsData.network = details.get("network");
 
-    // Get memo, paymentUrl, merchantData and requiredFeeRate
+    // Verify network is mainnet
+    detailsData.network = details.get("network");
     if (detailsData.network !== "main") {
       throw new Error("Network must be mainnet");
     }
 
+    // Sanity check time created is in the past
     const currentUnixTime = Math.floor(Date.now() / 1000);
     detailsData.time = details.get("time");
-    // Parse outputs as number amount and hex string script
 
-    if (
-      // if first output has SLP script, go to SLP flow.  Otherwise, BCH flow.
-      currentUnixTime < detailsData.time
-    ) {
+    if (currentUnixTime < detailsData.time) {
       throw new Error("Payment request time not valid");
     }
 
     detailsData.expires = details.get("expires");
-
     if (detailsData.expires < currentUnixTime) {
       throw new Error("Payment request expired");
     }
 
+    // Get memo, paymentUrl, merchantData and requiredFeeRate
     detailsData.memo = details.get("memo");
     detailsData.paymentUrl = details.get("payment_url");
+
     const merchantData = details.get("merchant_data");
     detailsData.merchantData = merchantData && merchantData.toString();
     detailsData.requiredFeeRate = details.get("required_fee_rate");
-    let tokenId = null;
-    let opReturnScript = null;
+
+    let tokenId: string | null = null;
+    let opReturnScript: string | null = null;
+
+    // Parse outputs as number amount and hex string script
     detailsData.outputs = details.get("outputs").map(
       (output: any, idx: number): OutputInfo => {
         let tokenAmount = null;
         const script = output.script.toString("hex");
 
-        if (
-          // ignore, expected for BCH payment requests
-          idx === 0
-        ) {
+        // if first output has SLP script, go to SLP flow.  Otherwise, BCH flow.
+        if (idx === 0) {
           const txOut = {
             vout: idx,
             tx: {
@@ -249,7 +248,7 @@ const decodePaymentRequest = async (requestData): Promise<PaymentRequest> => {
               e.message === "Not an SLP OP_RETURN"
             ) {
             } else {
-              // Calculate total output value and token amount
+              // ignore, expected for BCH payment requests
               console.warn(e);
             }
           }
@@ -280,10 +279,10 @@ const decodePaymentRequest = async (requestData): Promise<PaymentRequest> => {
           tokenId,
           tokenAmount
         };
-
-        // Calculate fee
       }
     );
+
+    // Calculate total output value and token amount
     let totalValue = new BigNumber(0);
     let totalTokenAmount = new BigNumber(0);
 
@@ -329,18 +328,16 @@ const signAndPublishPaymentRequestTransaction = async (
 
   let opReturnLength = null;
 
-  // Verify sufficient fee
+  // Add OP return length if present and not a token payment
   if (
     paymentRequest.outputs[0].amount.eq(0) &&
     !paymentRequest.outputs[0].tokenAmount
   ) {
     opReturnLength = Buffer.byteLength(paymentRequest.outputs[0].script, "hex");
-
-    // Destination outputs
   }
 
+  // Calculate fee
   for (const utxo of sortedSpendableUtxos) {
-    // Return remaining balance output
     if (utxo.spendable !== true) {
       throw new Error("Cannot spend unspendable utxo");
     }
@@ -349,27 +346,15 @@ const signAndPublishPaymentRequestTransaction = async (
     totalUtxoAmount += utxo.satoshis;
     inputUtxos.push(utxo);
     byteCount = SLP.BitcoinCash.getByteCount(
-      {
-        // send the payment transaction
-        P2PKH: inputUtxos.length
-      },
-
-      {
-        // calculate refund script pubkey
-        P2PKH: paymentRequest.outputs.length + 1
-      }
+      { P2PKH: inputUtxos.length },
+      { P2PKH: paymentRequest.outputs.length + 1 }
     );
 
-    // define the refund outputs
     if (opReturnLength) {
       byteCount += opReturnLength;
     }
 
-    if (
-      // serialize and send
-      totalUtxoAmount >=
-      byteCount + satoshisToSend
-    ) {
+    if (totalUtxoAmount >= byteCount + satoshisToSend) {
       break;
     }
   }
@@ -377,12 +362,14 @@ const signAndPublishPaymentRequestTransaction = async (
   // POST payment
   const satoshisRemaining = totalUtxoAmount - byteCount - satoshisToSend;
 
+  // Verify sufficient fee
   if (satoshisRemaining < 0) {
     throw new Error(
       "Not enough Bitcoin Cash for transaction fee. Deposit a small amount and try again."
     );
   }
 
+  // Destination outputs
   for (const output of paymentRequest.outputs) {
     transactionBuilder.addOutput(
       Buffer.from(output.script, "hex"),
@@ -390,6 +377,7 @@ const signAndPublishPaymentRequestTransaction = async (
     );
   }
 
+  // Return remaining balance output
   if (satoshisRemaining >= 546) {
     transactionBuilder.addOutput(from, satoshisRemaining);
   }
@@ -405,6 +393,8 @@ const signAndPublishPaymentRequestTransaction = async (
     );
   });
   const hex = transactionBuilder.build().toHex();
+
+  // send the payment transaction
   let payment = new PaymentProtocol().makePayment();
 
   paymentRequest.merchantData &&
@@ -414,11 +404,15 @@ const signAndPublishPaymentRequestTransaction = async (
     );
 
   payment.set("transactions", [Buffer.from(hex, "hex")]);
+
+  // calculate refund script pubkey
   const refundPubkey = SLP.ECPair.toPublicKey(refundKeypair);
   const refundHash160 = SLP.Crypto.hash160(Buffer.from(refundPubkey));
   const refundScriptPubkey = SLP.Script.pubKeyHash.output.encode(
     Buffer.from(refundHash160, "hex")
   );
+
+  // define the refund outputs
   let refundOutputs = [];
   let refundOutput = new PaymentProtocol().makeOutput();
   refundOutput.set("amount", 0);
@@ -426,21 +420,23 @@ const signAndPublishPaymentRequestTransaction = async (
 
   refundOutputs.push(refundOutput.message);
   payment.set("refund_to", refundOutputs);
-
   payment.set("memo", "");
+
+  // serialize and send
   const rawbody = payment.serialize();
   const headers = {
     Accept:
       "application/bitcoincash-paymentrequest, application/bitcoincash-paymentack",
     "Content-Type": "application/bitcoincash-payment",
-
     "Content-Transfer-Encoding": "binary"
   };
+
   const rawPaymentResponse = await postAsArrayBuffer(
     paymentRequest.paymentUrl,
     headers,
     rawbody
   );
+
   return rawPaymentResponse;
 };
 
@@ -486,7 +482,7 @@ const signAndPublishPaymentRequestTransactionSLP = async (
   if (scaledTokenSendAmount.lt(1)) {
     throw new Error(
       "Amount below minimum for this token. Increase the send amount and try again."
-    ); // +1 to receive remaining BCH
+    );
   }
 
   let tokenBalance = new BigNumber(0);
@@ -503,17 +499,13 @@ const signAndPublishPaymentRequestTransactionSLP = async (
     }
   }
 
-  // Verify sufficient fee
+  // Verify sufficient tokens
   if (!tokenBalance.gte(scaledTokenSendAmount)) {
     throw new Error("Insufficient tokens");
   }
-
-  // SLP data output
   const tokenChangeAmount = tokenBalance.minus(scaledTokenSendAmount);
 
-  // Token destination output
   if (tokenChangeAmount.isGreaterThan(0)) {
-    // Return remaining bch balance output
     to = [
       ...to,
       {
@@ -523,7 +515,6 @@ const signAndPublishPaymentRequestTransactionSLP = async (
     ];
   }
 
-  // calculate refund script pubkey from change address
   const sendOpReturn = slpjs.Slp.buildSendOpReturn({
     tokenIdHex: paymentRequest.tokenId,
     outputQtyArray: to.map(toInfo => toInfo.tokenAmount)
@@ -538,15 +529,11 @@ const signAndPublishPaymentRequestTransactionSLP = async (
     byteCount = SLPJS.calculateSendCost(
       sendOpReturn.length,
       inputUtxos.length,
-      to.length + 1,
+      to.length + 1, // +1 to receive remaining BCH
       tokenChangeAddress
     );
 
-    if (
-      inputSatoshis >= byteCount
-
-      // define the refund outputs
-    ) {
+    if (inputSatoshis >= byteCount) {
       break;
     }
   }
@@ -554,27 +541,29 @@ const signAndPublishPaymentRequestTransactionSLP = async (
   const transactionBuilder = new SLP.TransactionBuilder("mainnet");
   let totalUtxoAmount = 0;
 
-  // serialize and send
   inputUtxos.forEach(utxo => {
     transactionBuilder.addInput(utxo.txid, utxo.vout);
     totalUtxoAmount += utxo.satoshis;
   });
   const satoshisRemaining = totalUtxoAmount - byteCount;
 
-  // POST payment
+  // Verify sufficient fee
   if (satoshisRemaining < 0) {
     throw new Error(
       "Not enough Bitcoin Cash for fee. Deposit a small amount and try again."
     );
   }
 
+  // SLP data output
   transactionBuilder.addOutput(sendOpReturn, 0);
-
+  // Token destination output
   for (let toOutput of to) {
     transactionBuilder.addOutput(toOutput.address, 546);
   }
 
+  // Return remaining bch balance output
   transactionBuilder.addOutput(bchChangeAddress, satoshisRemaining + 546);
+
   let redeemScript;
   inputUtxos.forEach((utxo, index) => {
     transactionBuilder.sign(
@@ -594,6 +583,7 @@ const signAndPublishPaymentRequestTransactionSLP = async (
   payment.set("transactions", [Buffer.from(hex, "hex")]);
   const addressType = SLP.Address.detectAddressType(tokenChangeAddress);
   const addressFormat = SLP.Address.detectAddressFormat(tokenChangeAddress);
+
   let refundHash160 = SLP.Address.cashToHash160(tokenChangeAddress);
   let encodingFunc = SLP.Script.pubKeyHash.output.encode;
 
@@ -605,14 +595,20 @@ const signAndPublishPaymentRequestTransactionSLP = async (
     refundHash160 = SLP.Address.legacyToHash160(tokenChangeAddress);
   }
 
+  // calculate refund script pubkey from change address
   const refundScriptPubkey = encodingFunc(Buffer.from(refundHash160, "hex"));
   var refundOutputs = [];
   var refundOutput = new PaymentProtocol().makeOutput();
+
+  // define the refund outputs
   refundOutput.set("amount", 0);
   refundOutput.set("script", refundScriptPubkey);
   refundOutputs.push(refundOutput.message);
+
   payment.set("refund_to", refundOutputs);
   payment.set("memo", "");
+
+  // serialize and send
   const rawbody = payment.serialize();
   const headers = {
     Accept:
@@ -620,11 +616,14 @@ const signAndPublishPaymentRequestTransactionSLP = async (
     "Content-Type": "application/simpleledger-payment",
     "Content-Transfer-Encoding": "binary"
   };
+
+  // POST payment
   const rawPaymentResponse = await postAsArrayBuffer(
     paymentRequest.paymentUrl,
     headers,
     rawbody
   );
+
   return rawPaymentResponse;
 };
 
