c9135984c67abb3d40e0929c3a97c7509c542943
Peter Zenger <peter.zenger1@gmail.com>
2020-03-03 19:01:43 -0800

    Token redux test progress

--- data/tokens/reducer.test.ts
+++ data/tokens/reducer.test.ts
@@ -1,4 +1,4 @@
-import tokensReducer, { initialState } from "./reducer";
+import tokensReducer, { initialState, TokenData } from "./reducer";
 import {
   updateTokensMetaStart,
   updateTokensMetaSuccess,
@@ -29,8 +29,118 @@ describe("tokens::reducer", () => {
   });
 
   describe("updating token metadata success", () => {
-    it("updates a single tokens metadata", () => {});
-    it("updates multiple tokens metadata at a time", () => {});
-    it("does not add a token multiple times and handles null values", () => {});
+    it("updates a single tokens metadata", () => {
+      const stateBefore = { ...initialState };
+
+      const tokenData: TokenData[] = [
+        {
+          tokenId:
+            "29d353a3d19cdd7324f1c14b3fe289293976842869fed1bea3f9510558f6f006",
+          symbol: "LEAD",
+          name: "LEAD Token (Leaders of Education Adoption and Development)",
+          decimals: 2,
+          protocol: "slp"
+        }
+      ];
+
+      const stateAfter = tokensReducer(
+        stateBefore,
+        updateTokensMetaSuccess(tokenData)
+      );
+
+      const expectedState = {
+        ...initialState,
+        byId: {
+          "29d353a3d19cdd7324f1c14b3fe289293976842869fed1bea3f9510558f6f006": {
+            tokenId:
+              "29d353a3d19cdd7324f1c14b3fe289293976842869fed1bea3f9510558f6f006",
+            symbol: "LEAD",
+            name: "LEAD Token (Leaders of Education Adoption and Development)",
+            decimals: 2,
+            protocol: "slp"
+          }
+        },
+        allIds: [
+          "29d353a3d19cdd7324f1c14b3fe289293976842869fed1bea3f9510558f6f006"
+        ],
+        updating: false
+      };
+
+      expect(stateAfter).toEqual(expectedState);
+
+      // Confirm adding same token again does not change anything
+      const stateAfter2 = tokensReducer(
+        stateAfter,
+        updateTokensMetaSuccess(tokenData)
+      );
+      expect(stateAfter2).toEqual(expectedState);
+    });
+    it("updates multiple tokens metadata at a time", () => {
+      const stateBefore = { ...initialState };
+
+      const tokenData: TokenData[] = [
+        {
+          tokenId:
+            "29d353a3d19cdd7324f1c14b3fe289293976842869fed1bea3f9510558f6f006",
+          symbol: "LEAD",
+          name: "LEAD Token (Leaders of Education Adoption and Development)",
+          decimals: 2,
+          protocol: "slp"
+        },
+        {
+          tokenId:
+            "f66c6d0ac6b8c5c4ed469234ec9734f6d3499b0351b22349f40e617d22254fec",
+          symbol: "zBCH",
+          name: "Zurich BCH Meetup Token",
+          decimals: 8,
+          protocol: "slp"
+        }
+      ];
+
+      const stateAfter = tokensReducer(
+        stateBefore,
+        updateTokensMetaSuccess(tokenData)
+      );
+
+      const expectedState = {
+        ...initialState,
+        byId: {
+          "29d353a3d19cdd7324f1c14b3fe289293976842869fed1bea3f9510558f6f006": {
+            tokenId:
+              "29d353a3d19cdd7324f1c14b3fe289293976842869fed1bea3f9510558f6f006",
+            symbol: "LEAD",
+            name: "LEAD Token (Leaders of Education Adoption and Development)",
+            decimals: 2,
+            protocol: "slp"
+          },
+          f66c6d0ac6b8c5c4ed469234ec9734f6d3499b0351b22349f40e617d22254fec: {
+            tokenId:
+              "f66c6d0ac6b8c5c4ed469234ec9734f6d3499b0351b22349f40e617d22254fec",
+            symbol: "zBCH",
+            name: "Zurich BCH Meetup Token",
+            decimals: 8,
+            protocol: "slp"
+          }
+        },
+        allIds: [
+          "29d353a3d19cdd7324f1c14b3fe289293976842869fed1bea3f9510558f6f006",
+          "f66c6d0ac6b8c5c4ed469234ec9734f6d3499b0351b22349f40e617d22254fec"
+        ],
+        updating: false
+      };
+
+      expect(stateAfter).toEqual(expectedState);
+    });
+
+    it("does nothing if an empty array of tokens is passed in", () => {
+      const stateBefore = { ...initialState };
+      const stateAfter = tokensReducer(
+        stateBefore,
+        updateTokensMetaSuccess([])
+      );
+
+      const expectedState = { ...initialState };
+      expect(stateAfter).toEqual(expectedState);
+    });
   });
 });
